b200f6541e8c280d297d8e133da265d9d3900b29
ASUS <kdv1990kdv@bk.ru>
2021-06-10 14:30:21 +0300

    bootstrap

--- src/main/java/com/controllers/AdminController.java
+++ src/main/java/com/controllers/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 
